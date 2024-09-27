7ba9373373b507a7ba7703a1ae619d5eec6d83ce
Joyal <georgejoyal31@gmail.com>
2024-09-28 00:36:19 +0530

    deploying product in github pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Task-Flow/'
+  base: '/Task-Flow/', // This should match the GitHub Pages path
 })

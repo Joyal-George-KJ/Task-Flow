d681ac3e0477878ca092fd8a872167e841568a2c
Joyal <georgejoyal31@gmail.com>
2024-10-06 03:26:29 +0530

    Added React Lazy for Better Performance!

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,47 @@
-import React from "react";
-import Home from "./pages/Home";
-import { Routes, Route, HashRouter } from 'react-router-dom'
-import Layout from "./pages/Layout";
-import Projects from "./pages/Projects";
-import Task from "./components/Task";
+import React, { lazy, Suspense } from "react";
+import { Routes, Route, HashRouter } from "react-router-dom";
+
+const LazyProjects = lazy(() => import("./pages/Projects"));
+const LazyLayout = lazy(() => import("./pages/Layout"));
+const LazyTask = lazy(() => import("./components/Task"));
+const LazyHome = lazy(() => import("./pages/Home"));
 
 function App() {
     return (
         <HashRouter>
             <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="/project" element={<Projects />} />
-                    <Route path="/task/:id" element={<Task />} />
+                <Route
+                    path="/"
+                    element={
+                        <Suspense fallback="Loading...">
+                            <LazyLayout />
+                        </Suspense>
+                    }
+                >
+                    <Route
+                        index
+                        element={
+                            <Suspense fallback="Loading...">
+                                <LazyHome />
+                            </Suspense>
+                        }
+                    />
+                    <Route
+                        path="/project"
+                        element={
+                            <Suspense fallback="Loading...">
+                                <LazyProjects />
+                            </Suspense>
+                        }
+                    />
+                    <Route
+                        path="/task/:id"
+                        element={
+                            <Suspense fallback="Loading...">
+                                <LazyTask />
+                            </Suspense>
+                        }
+                    />
                 </Route>
             </Routes>
         </HashRouter>
